6348edeaadae9242f216b95a0ca59126259eebb7
Brandin Canfield <brandincanfield@gmail.com>
2018-04-05 22:00:44 +0000

    Client works now!!!!
    woohoo

--- utils/client.py
+++ utils/client.py
@@ -4,7 +4,7 @@ import sys
 import socket 
  
 skClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-skClient.connect(("127.0.0.1",2525))
+skClient.connect(("10.1.39.102",8085))
  
 sFileName = raw_input("Enter Filename to download from server : ")
 sData = "Temp"
@@ -12,11 +12,12 @@ sData = "Temp"
 while True:
     skClient.send(sFileName)
     sData = skClient.recv(1024)
-    fDownloadFile = open(sFileName,"wb")
+    fDownloadFile = open(sFileName,"w")
     while sData:
+        print sData
         fDownloadFile.write(sData)
         sData = skClient.recv(1024)
     print "Download Completed"
     break
- 
-skClient.close()
+fDownloadFile.close()
+skClient.close(
