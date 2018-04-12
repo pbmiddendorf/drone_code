3069254b077ce3cac386a1ce0e792dd8e8124420
Paul <middendorf.7@wright.edu>
2018-04-12 16:28:19 -0400

    Added control block to speed up / slow down bottom motor using keyboard

--- utils/three_motor_control_test.py
+++ utils/three_motor_control_test.py
@@ -1,6 +1,21 @@
 import pigpio #allows nice PWM output on almost any pin, incl hardware pwm if necessary
-from time import sleep
-bottomMotor = 5 #this is physical pin 7, gpio pins are designated by their BCM number
+#import sys
+#import tty
+#import time
+import msvcrt
+#import atexit
+
+
+def kbfunc():
+    x = msvcrt.kbhit()
+    if x:
+        ret = ord(msvcrt.getch())
+    else:
+        ret = 0
+    return ret
+
+
+bottomMotor = 5 #this is physical pin #, gpio pins are designated by their BCM number
 rightMotor = 13
 leftMotor = 26
 outputPulseWidth = 1000 #1000 is the minimum, ie not moving
@@ -12,18 +27,34 @@ pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth) #sends the command to the
 pi.set_servo_pulsewidth(leftMotor, outputPulseWidth)
 pi.set_servo_pulsewidth(rightMotor, outputPulseWidth)
 
-for i in range(1, 300):
-    outputPulseWidth = outputPulseWidth + 1
-    pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth)
-    pi.set_servo_pulsewidth(leftMotor, outputPulseWidth)
-    pi.set_servo_pulsewidth(rightMotor, outputPulseWidth)
-    sleep(0.1)
+if __name__ == '__main__':
 
-for i in range(1, 300):
-    outputPulseWidth = outputPulseWidth - 1
-    pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth)
-    pi.set_servo_pulsewidth(leftMotor, outputPulseWidth)
-    pi.set_servo_pulsewidth(rightMotor, outputPulseWidth)
-    sleep(0.1)
+    while True:
+        if msvcrt.kbhit():
+            char = ord(msvcrt.getch())
+            print(char)
+            if char == 119:
+                outputPulseWidth = outputPulseWidth+5
+                pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth)
+            if char == 115:
+                outputPulseWidth = outputPulseWidth - 5
+                pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth)
 
-pi.stop()
+
+
+
+# for i in range(1, 300):
+#     outputPulseWidth = outputPulseWidth + 1
+#     pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth)
+#     pi.set_servo_pulsewidth(leftMotor, outputPulseWidth)
+#     pi.set_servo_pulsewidth(rightMotor, outputPulseWidth)
+#     sleep(0.1)
+#
+# for i in range(1, 300):
+#     outputPulseWidth = outputPulseWidth - 1
+#     pi.set_servo_pulsewidth(bottomMotor, outputPulseWidth)
+#     pi.set_servo_pulsewidth(leftMotor, outputPulseWidth)
+#     pi.set_servo_pulsewidth(rightMotor, outputPulseWidth)
+#     sleep(0.1)
+
+#pi.stop()
